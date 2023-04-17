31fb210969cf6f058f692aa872425c5cfb3718aa
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-17 18:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function strip(bandname){
 	return bandname.replace(/^(a|the|an)/i,'').trim();
 	
 }
-const sortedBands=bands.sort((a,b)=>strip(a)>strip(b)?1:-1);
+const sortedBands=touristSpots.sort((a,b)=>strip(a)>strip(b)?1:-1);
 document.querySelector('#bands').innerHTML=sortedBands.map(band=>
 	`<li>${band}</li>`).join(``)
 console.log(sortedBands);
