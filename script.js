bd6941c5c3b50db5d1af0a497e6476dc5c6596e2
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-19 15:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //your code here
 
 let touristSpots=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-function strip(bandname){
-	return bandname.replace(/^(a|the|an)/i,'').trim();
-	
+function removeArticles(touristSpots) {
+  words = touristSpots.split(" ");
+  if(words.length <= 1) return touristSpots;
+  if( words[0] == 'a' || words[0] == 'the' || words[0] == 'an' )
+    return words.splice(1).join(" ");
+  return touristSpots;
 }
-const sortedBands=touristSpots.sort((a,b)=>strip(a)>strip(b)?1:-1);
-document.querySelector('#bands').innerHTML=sortedBands.map(band=>
-	`<li>${band}</li>`).join(``)
-console.log(sortedBands);
+	
+
