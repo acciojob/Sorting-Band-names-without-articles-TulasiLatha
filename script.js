631ad34a363c94d61562904dc7b720f0f6294e8d
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-24 13:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,33 @@
 
 let touristSpots=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 function removeArticles(touristSpots) {
-  words = touristSpots.split(" ");
-  if(words.length <= 1) return touristSpots;
-  if( words[0] == 'a' || words[0] == 'the' || words[0] == 'an' )
-    return words.splice(1).join(" ");
-  return touristSpots;
+//   words = touristSpots.split(" ");
+//   if(words.length <= 1) return touristSpots;
+//   if( words[0] == 'a' || words[0] == 'the' || words[0] == 'an' )
+//     return words.splice(1).join(" ");
+//   return touristSpots;
+// }
+	for (const article of articles) {
+    if (touristSpots.toLowerCase().startsWith(article)) {
+      return touristSpots.slice(article.length).trim();
+    }
+  }
+  return touristSpots.trim();
 }
+const sortedBandNames = bandNames.sort((a, b) => removeArticle(a) > removeArticle(b) ? 1 : -1);
+
+// create the HTML list
+let htmlList = '<ul id="band">';
+sortedBandNames.forEach(name => {
+  htmlList += `<li>${name}</li>`;
+});
+htmlList += '</ul>';
+
+console.log(htmlList);
+
+
+
+
+
+
 	
